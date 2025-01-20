b11a872095e02c22c518f080dde34fb6a6fe9847
Thanh Do <90175211+TxSew@users.noreply.github.com>
2025-01-20 18:39:45 +0700

    update appConfig

--- src/configs/AppConfig.ts
+++ src/configs/AppConfig.ts
@@ -2,10 +2,10 @@ const env = import.meta.env;
 
 export const appConfig = {
     service: {
-        api: env.VITE_NUMBOT_API_URL || 'http://localhost:8000/api',
+        api: 'https://wf96ndf0-8000.asse.devtunnels.ms/api' || 'http://localhost:8000/api',
     },
     url:{
-        pathBotProgress: env.VITE_PATH_STATIC_NUMBOTS_BOT_PROGRESS_URL!,
-        pathStaticMedia: env.VITE_PATH_PLAY_NUMBOTS_STATIC_MEDIA_URL!,
+        pathBotProgress: 'https://static.numbots.com/data/bot-progress',
+        pathStaticMedia: 'https://play.numbots.com/static/media',
     }
 };
