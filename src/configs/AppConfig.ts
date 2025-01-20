eb1696b342cd639639b0e51a43a97688251fa14b
Thanh Do <90175211+TxSew@users.noreply.github.com>
2025-01-20 18:47:04 +0700

    update env

--- src/configs/AppConfig.ts
+++ src/configs/AppConfig.ts
@@ -1,7 +1,7 @@
 
 export const appConfig = {
     service: {
-        api: 'https://wf96ndf0-8000.asse.devtunnels.ms/api' || 'http://localhost:8000/api',
+        api: `https://wf96ndf0-8000.asse.devtunnels.ms/api`
     },
     url:{
         pathBotProgress: 'https://static.numbots.com/data/bot-progress',
